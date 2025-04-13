e84dc0ae6e8537615237fa51d89d40b9892cc695
Lumine ID <LumineId>
2025-04-14 02:20:47 +0700

    optimize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const usedClass = ["bg", "text", "border", "bg-outliine"]
+const usedClassColor = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "purple",
+  "pink",
+  "cyan",
+  "teal"
+]
+
 export default {
   content: ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue", "*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist:(usedClass.map(
+    cls =>
+      usedClassColor.map(
+        color => `${cls}-${color}-400`
+      )
+  )).flat()
 }
 
